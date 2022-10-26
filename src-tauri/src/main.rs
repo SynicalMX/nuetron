5a97c96fd26e520f00f74e38fc95db5f6b1801b6
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2022-10-26 08:48:51 -0400

    test commit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,17 +4,19 @@
 )]
 
 mod types;
+use types::Position
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    let pos = 
-    format!("Hello, {}! ", name)
+fn create(node: str) {
+    if (node === "Position") {
+        return Position::new()
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![create])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
